3f2b7a8321f255a8effded54e51ede75a289d9ee
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2025-08-08 10:57:59 +0800

    Update mt76x8.mk

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -382,7 +382,7 @@ endef
 TARGET_DEVICES += totolink_a3
 
 define Device/totolink_lr1200
-  IMAGE_SIZE := 7872k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := TOTOLINK
   DEVICE_MODEL := LR1200
   DEVICE_PACKAGES := kmod-mt76x2 kmod-usb2 uqmi
